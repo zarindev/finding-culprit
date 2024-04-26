f53bbe9c913acacd8f19eb74dfa8e7a073155bbc
zarindev <masterofalltradezarin@gmail.com>
2024-04-26 09:31:07 +0600

    credentials

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -5,13 +5,13 @@ test("Login Setup", async({page}) => {
 
   await page.goto("https://account.tallyfy.com/login");
 
-  await page.fill('input[type="email"]', `${process.env.EMAIL_ID}`);
+  await page.fill('input[type="email"]', "mdzarinjerry@gmail.com");
 
-  await page.fill('input[type="password"]', `${process.env.PASSWORD}`);
+  await page.fill('input[type="password"]', "A@12345b");
 
   await page.click('button[type="submit"]');
 
-  await page.waitForURL(`https://go.tallyfy.com/${process.env.ORG_ID}/dashboard`);
+  await page.waitForURL(`https://go.tallyfy.com/9710244f43f053c2618dd09613e3c56d/dashboard`);
 
   await expect(page).toHaveTitle(/Tallyfy/);
 

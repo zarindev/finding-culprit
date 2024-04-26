b3275552e425b2cfacd42010b54a5c46d728f64f
zarindev <masterofalltradezarin@gmail.com>
2024-04-26 10:23:41 +0600

    task and document creation

--- tests/actionsTest.spec.ts
+++ tests/actionsTest.spec.ts
@@ -1,7 +1,23 @@
 import { test, expect, chromium } from "@playwright/test";
 import { TaskCreation } from "../pages/actions/taskCreation";
+import { DocumentTemplateCreation } from "../pages/actions/documentTemplateCreation";
 
 test("Create a Task From Create Modal", async ({ page }) => {
     const Task = new TaskCreation(page);
     await Task.taskCreateFromCreateModal();
 });
+
+test("Create a Task from Tasks Page", async ({ page }) => {
+    const Task = new TaskCreation(page);
+    await Task.taskCreate();
+  });
+  
+  test("Create a Task from Home Page", async ({ page }) => {
+      const Task = new TaskCreation(page);
+      await Task.taskCreateFromHome();
+  })
+  
+  test("Document Template creation", async ({ page }) => {
+    const DocTemp = new DocumentTemplateCreation(page);
+    await DocTemp.createDocTemplate();
+  });

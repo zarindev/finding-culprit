0d5fddca8731f1167e804fba380bafb0409762ed
zarindev <masterofalltradezarin@gmail.com>
2024-04-26 10:06:37 +0600

    create btn navigation

--- tests/navigationTest.spec.ts
+++ tests/navigationTest.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect, chromium } from "@playwright/test";
 import { NavigateToHomePage } from "../pages/navigations/navigateToHomePage";
+import { NavigateToCreateBtnModal } from "../pages/navigations/navigateToCreateBtnModal";
+
+test("Navigate to the Create Button Modal", async ({page}) => {
+    const navigateToCreateBtnModal = new NavigateToCreateBtnModal(page);
+    await navigateToCreateBtnModal.navigateToCreateBtnModal();
+});
 test("Navigate to the Home Page", async ({page}) => {
     const navigateToHomePage = new NavigateToHomePage(page);
     await navigateToHomePage.navigateToHomePage();
